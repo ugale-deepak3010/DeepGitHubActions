5812b45e334ce61b5c89f7dc6dc376442822d641
Dipak Ugale <ugale.deepak3010@gmail.com>
2025-02-18 15:55:38 +0530

    Update addition.py

--- src/addition.py
+++ src/addition.py
@@ -1,11 +1,11 @@
 def add(a, b):
-  int r=a+b;
-  print("\n returning addition as: ");
-  print(r);
+  print(" returning addition as: ");
+  print(a+b);
   return a+b;
 
 def test_add():
   assert add(1,2)==3;
   assert add(1,-1)==0;
 
+add(2,2);
 #run the project after pushing.
